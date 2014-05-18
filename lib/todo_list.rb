a744ebe8d1d69bc549308e22333c62e90fc49350
akuraru <akuraru@gmail.com>
2014-05-18 14:09:26 +0900

    新規作成

---
+++ lib/todo_list.rb
@@ -0,0 +1,5 @@
+require "todo_list/version"
+
+module TodoList
+  # Your code goes here...
+end
---
+++ spec/todo_list_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe TodoList do
+  it 'has a version number' do
+    expect(TodoList::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
