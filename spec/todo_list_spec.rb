2ab22353d7b718e7d7593df4e3371f2865c76597
akuraru <akuraru@gmail.com>
2014-05-18 14:41:36 +0900

    リファクタリング

--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -4,15 +4,21 @@ describe TodoList do
   it 'has a version number' do
     expect(TodoList::VERSION).not_to be nil
   end
-  it "TODOを追加する" do
-  	list = TaskList.new
-  	list.add(Task.new(description: 'description'))
-  	expect(list.count).to eq 1
- 	end
- 	it "2つ追加してカウントが２になる" do
-  	list = TaskList.new
-  	list.add(Task.new(description: 'description'))
-  	list.add(Task.new(description: 'description'))
-  	expect(list.count).to eq 2
+
+  describe '#add' do
+  	let(:list) { TaskList.new }
+
+    context '1件追加される場合' do
+	    before { list.add(Task.new(description: 'description')) }
+  	  it "リストのカウントが1になる" do
+	  	  expect(list.count).to eq 1
+	 	  end
+	    context 'さらに追加される場合' do
+		    before { list.add(Task.new(description: 'description')) }
+			 	it "リストのカウントが２になる" do
+			  	expect(list.count).to eq 2
+			  end
+		  end
+    end
   end
 end
